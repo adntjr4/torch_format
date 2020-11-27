5ebf8b978b3884adab54e78a00548922445eb50d
woos <adntjr4@gmail.com>
2020-11-27 16:12:56 +0900

    modify test.py and delete evaluate.py

--- test.py
+++ test.py
@@ -1,55 +1,33 @@
-import argparse, os
+import argparse
 
-from torch.utils.data import DataLoader
+import torch
 
-from src.util.util import load_model
 from src.util.config_parse import ConfigParser
-from src.data_set.data_set import DataSet, batch_collate
-from src.model.base import BaseModel
-from src.trainer.evaluator import Evaluator
+from src.trainer.trainer import Trainer
 
 
-def main(config):
-    # gpu
-    os.environ["CUDA_VISIBLE_DEVICES"] = config['device']
+def main():
+    # parsing configuration
+    args = argparse.ArgumentParser()
+    args.add_argument('--session_name', default=None,  type=str)
+    args.add_argument('--config',  default=None,  type=str)
+    args.add_argument('--resume',  default=False, type=bool)
+    args.add_argument('--gpu',     default=None,  type=str)
+    args.add_argument('--thread',  default=4,     type=int)
 
-    # data loader
-    conf_dl= config['data_loader']
-    
-    num_workers = conf_dl['num_workers']
+    args = args.parse_args()
 
-    batch_size = conf_dl['batch_size'] if config['batch_size'] == -1 else config['batch_size']
-    test_data_set = DataSet(conf_dl, mode='val', human_only=True)
-    test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=config['shuffle'], num_workers=num_workers, collate_fn=batch_collate)
+    assert args.session is not None, 'session name required'
+    assert args.config is not None, 'config file path is needed'
 
-    # model
-    model = BaseModel(config['model'])
-    model.load_state_dict(load_model(config['weight']))
+    cfg = ConfigParser(args)
 
-    # evaluator
-    evaluator = Evaluator(model, test_data_loader, test_data_set, config)
+    # intialize trainer
+    trainer = Trainer(cfg)
 
-    # image out
-    evaluator.image_prediction()
+    # test
+    trainer.test()
 
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser()
-    args.add_argument('-c', '--config', default=None, type=str)
-    args.add_argument('-w', '--weight', default=None)
-    args.add_argument('-d', '--device', default='0', type=str)
-    args.add_argument('-b', '--batch_size', default=-1, type=int)
-    args.add_argument('--shuffle', action='store_true')
-    args.add_argument('-n', '--img_num', default=1, type=int)
-    args.add_argument('--img_name', default='test', type=str)
-    
-    args = args.parse_args()
-
-    args.weight = 'data/saved/checkpoint/R50_human_checkpoint.pth'
-
-    assert args.config is not None, 'config file path is needed'
-    assert args.weight is not None, 'model weight is needed for evaluation'
-
-    config = ConfigParser(args)
-
-    main(config)
+    main()
